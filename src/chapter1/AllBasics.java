e01fcba6429e4682e8bf50c9d2fe4c5a3a1d7f62
femyjoy <femyraechel@gmail.com>
2019-02-19 23:11:02 +0000

    Initial commit

---
+++ src/chapter1/AllBasics.java
@@ -0,0 +1,10 @@
+package chapter1;
+
+public class AllBasics {
+    public static void main(String[] args) {
+        if(args.length>0){
+         for(String arg:args)
+             System.out.println(arg);
+        }
+    }
+}
